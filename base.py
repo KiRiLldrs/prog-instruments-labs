642ee832700a1d5a61ad56cfe0491246619b5d57
Kirill <kirillstengauer@gmail.com>
2025-12-14 19:35:08 +0400

    Add logger to the Base class

--- base.py
+++ base.py
@@ -1,6 +1,21 @@
 from abc import ABC, abstractmethod
+import logging
 
 class BaseTextClassifier(ABC):
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
+
+        if not self.logger.handlers:
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter(
+                "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S"
+            )
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+
+
     @abstractmethod
     def fit(self, df, csv_path, test_size): ...
 
