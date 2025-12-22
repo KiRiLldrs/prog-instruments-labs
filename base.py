7cc981a546c3a142a28ec625695afed1576f2f2d
Kirill <kirillstengauer@gmail.com>
2025-12-22 20:31:42 +0400

    add requirements.txt

---
+++ base.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+import logging
+
+class BaseTextClassifier(ABC):
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.INFO)
+
+        self.logger.propagate = False
+
+        if not self.logger.handlers:
+            handler = logging.StreamHandler()
+            formatter = logging.Formatter(
+                "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S"
+            )
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+
+
+    @abstractmethod
+    def fit(self, df, csv_path, test_size): ...
+
+    @abstractmethod
+    def predict(self, texts): ...
+
+    @abstractmethod
+    def save(self, path): ...
